3a3af9a05483480658b2ae1cbf70400bd2006831
Igor Trentini <igortrentini.2004@gmail.com>
2022-09-16 13:32:20 -0300

    wip: Estilizando frontend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,17 @@
+import './styles/main.css';
+
+import logoSvg from './assets/logo-nlw-esports.svg';
+
 export const App = () => {
-    return <div>hello world</div>;
+    return (
+        <div className="max-w-[1344] mx-auto flex flex-col items-center my-20">
+            <img src={logoSvg} alt="Logo do NLW eSports" />
+
+            <h1 className="text-6xl text-white font-black mt-20">
+                Seu
+                <span className="text-transparent bg-nlw-gradient bg-clip-text"> duo </span>
+                está aqui
+            </h1>
+        </div>
+    );
 };
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx', './index.html'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 48.94%, #E1D55D 33.57%)'
+      }
+    },
+  },
+  plugins: [],
+}
